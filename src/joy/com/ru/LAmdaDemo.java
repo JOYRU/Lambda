cd117bf5ba012f58ede37385551318468c512194
JOY_RU <joycseru@gmail.com>
2017-11-30 12:18:16 +0600

    test LambaDemo class with interface MyNum

---
+++ src/joy/com/ru/LAmdaDemo.java
@@ -0,0 +1,16 @@
+package joy.com.ru;
+
+public class LAmdaDemo {
+
+	public static void main(String[] args) {
+		Mynum mynum ; 
+		
+		mynum = (n) ->(n%2)==0 ; 
+		
+		
+		System.out.println(mynum.getValue(10)) ;
+		System.out.println(mynum.getValue(111)) ;
+		
+	}
+
+}
